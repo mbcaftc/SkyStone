b8b165dedad9715c2d0b30616cc48e399acd82ce
emma.acker <emma.acker@mtbethelchrisitan.org>
2019-09-09 20:27:00 -0400

    Emma - added the packages (DriveTrains and Controls) into the Outreach package
    Emma - created two new classes: RoverTeleOp in the Controls package and RoverDrive in the DriveTrains package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Controls/RoverTeleOp.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.Outreach.Controls;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Outreach.DriveTrains.RoverDrive;
+
+
+    @TeleOp(name = "Rover Drive")
+    public class RoverTeleOp extends OpMode {
+
+        RoverDrive myRover;
+
+
+        public void init(){
+
+            myRover = new RoverDrive(hardwareMap.dcMotor.get("front_left_motor"), hardwareMap.dcMotor.get("front_right_motor"), hardwareMap.dcMotor.get("rear_left_motor"), hardwareMap.dcMotor.get("rear_right_motor"));
+
+
+        }
+
+        public void loop () {
+
+            drive();
+            telemetry();
+        }
+
+
+//methods
+
+        public void drive() {
+            if(gamepad1.dpad_up) {      // code that works
+
+                if (gamepad1.left_stick_y > .1) {
+                    myRover.driveForward(gamepad1.left_stick_y);
+                } else if (gamepad1.left_stick_y < -.1) {
+                    myRover.driveBackward(gamepad1.left_stick_y);
+                } else if (gamepad1.left_stick_x > .1) {
+                    myRover.rotateLeft(gamepad1.left_stick_x);
+                } else if (gamepad1.left_stick_x < -.1) {
+                    myRover.rotateRight(gamepad1.left_stick_x);
+                } else {
+                    myRover.stopMotors();
+                }
+            }
+            else if (gamepad1.dpad_down) {              // experimental code
+                if (gamepad1.left_stick_y > .1) {
+                    myRover.driveForward(gamepad1.left_stick_y);
+                } else if (gamepad1.left_stick_y < -.1) {
+                    myRover.driveBackward(gamepad1.left_stick_y);
+                } else if (gamepad1.left_stick_x > .1) {
+                    myRover.rotateLeft(gamepad1.left_stick_x);
+                } else if (gamepad1.left_stick_x < -.1) {
+                    myRover.rotateRight(gamepad1.left_stick_x);
+                } else {
+                    myRover.stopMotors();
+                }
+            }
+        }
+
+        public void telemetry () {
+
+            telemetry.addData("y coordinate", gamepad1.left_stick_y);
+            telemetry.addData("x coordinate", gamepad1.left_stick_x);
+
+            telemetry.update();
+        }
+
+
+
+    }
+
