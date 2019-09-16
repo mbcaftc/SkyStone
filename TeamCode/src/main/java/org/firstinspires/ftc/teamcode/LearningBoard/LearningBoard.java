08e085375d4af276d6dde0f27b74eb2207235db0
DawsonK2019 <dawson.kelani@mtbethelchristian.org>
2019-09-16 16:59:58 -0400

    Kelani's test example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningBoard/LearningBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningBoard/LearningBoard.java
@@ -1,54 +1,126 @@
 package org.firstinspires.ftc.teamcode.LearningBoard;
 
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "learning board")
-public class LearningBoard extends OpMode {
-    public Servo servo_1, servo_2;
-    public DcMotor motor_1, motor_2, linear_slide_motor;
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
 
-    @Override
+@TeleOp (name = "learning board")
+public class LearningBoard extends OpMode {
+
+    DcMotor motor1;
+    DcMotor motor2;
+    DcMotor linearSlideMotor;
+
+    Servo servo1;
+    Servo servo2;
+
+    RevTouchSensor touchSensor;
+    SensorREVColorDistance disColSensor;
+
     public void init() {
-        servo_1 = hardwareMap.servo.get("servo_1");
-        servo_2 = hardwareMap.servo.get("servo_2");
-        motor_1 = hardwareMap.dcMotor.get("motor_1");
-        motor_2 = hardwareMap.dcMotor.get("motor_2");
-        linear_slide_motor = hardwareMap.dcMotor.get("linear_slide_motor");
-        motor_1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor_2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        linear_slide_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        linear_slide_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motor1 = hardwareMap.dcMotor.get("motor_1");
+        motor2 = hardwareMap.dcMotor.get("motor_2");
+        servo1 = hardwareMap.servo.get("servo_1");
+        servo1.setDirection(Servo.Direction.FORWARD);
+        servo1.setDirection(Servo.Direction.REVERSE);
+        servo2 = hardwareMap.servo.get("servo_2");
+        servo2.setDirection(Servo.Direction.FORWARD);
+        servo2.setDirection(Servo.Direction.REVERSE);
+        linearSlideMotor = hardwareMap.dcMotor.get("linear_slide_motor");
+
+        touchSensor = hardwareMap.get(RevTouchSensor.class, "touch_sensor");
+        disColSensor = hardwareMap.get(SensorREVColorDistance.class, "rev_distance_color_sensor");
+
+
     }
 
-    @Override
     public void loop() {
-        if (gamepad1.a) {
-            servo_1.setPosition(1);
+
+        motor1Control();
+        motorControl2();
+        servo1Control();
+        servo2Control();
+
+
+
+    }
+
+
+    public void motor1Control() {
+
+        if (gamepad1.left_stick_y < -.1) {
+            motor1.setPower(gamepad1.left_stick_y);
+        } else if (gamepad1.left_stick_y > .1) {
+            motor1.setPower(gamepad1.left_stick_y);
+        } else {
+            motor1.setPower(0);
         }
-        else if (gamepad1.b){
-            servo_1.setPosition(0);
+    }
+
+    public void motorControl2() {
+
+        if (gamepad1.left_stick_y < -.1) {
+            motor2.setPower(gamepad1.right_stick_y);
+        } else if (gamepad1.right_stick_y > .1) {
+            motor2.setPower(gamepad1.right_stick_y);
+        } else {
+            motor2.setPower(0);
         }
-        else {
-            servo_1.setPosition(.5);
-        }
-        if (gamepad1.x) {
-            servo_2.setPosition(1);
-        }
-        else if (gamepad1.y){
-            servo_2.setPosition(0);
-        }
-        else {
-            servo_2.setPosition(.5);
+    }
+
+    public void linearSlideControl () {
+        if (gamepad1.dpad_up == true) {
+            linearSlideMotor.setPower(1);
+        } else if (gamepad1.dpad_down ==true) {
+            linearSlideMotor.setPower(-1);
+        } else {
+            linearSlideMotor.setPower(0);
         }
 
-        motor_1.setPower(gamepad1.left_stick_y);
-        motor_2.setPower(gamepad1.right_stick_y);
-        linear_slide_motor.setPower(gamepad1.right_trigger);
-        linear_slide_motor.setPower(-gamepad1.left_trigger);
+    }
+    public void servo1Control () {
+        if (gamepad1.y ==true) {
+            servo1.setPosition(0.5);
+        } else if (gamepad1.x ==true) {
+            servo1.setPosition(0.75);
+        } else {
+
+        }
+    }
+    public void servo2Control () {
+        if (gamepad1.b ==true) {
+            servo2.setPosition(0.25);
+        } else if (gamepad1.a ==true) {
+            servo2.setPosition(0.4);
+        } else {
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
