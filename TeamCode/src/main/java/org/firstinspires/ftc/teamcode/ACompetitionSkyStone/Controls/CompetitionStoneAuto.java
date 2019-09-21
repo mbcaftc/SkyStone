daab1b907dd1bedcbc1ea9ba0be66fcb3f09ff48
emma.acker <emma.acker@mtbethelchrisitan.org>
2019-09-21 12:41:01 -0400

    Emma - started woring on competition code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ACompetitionSkyStone/Controls/CompetitionStoneAuto.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.ACompetitionSkyStone.Controls;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.internal.android.dx.dex.file.MethodAnnotationStruct;
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.DriveTrains.MecanumDrive;
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.subsystems.IntakeSpinner;
+
+@Autonomous (name = "Comp Auto Gen 1")
+public class CompetitionStoneAuto extends LinearOpMode {
+
+    MecanumDrive mechDrive;
+    IntakeSpinner intakeSpin;
+    @Override
+
+
+
+
+
+    public void runOpMode() throws InterruptedException {
+      mechDrive = new MecanumDrive (hardwareMap.dcMotor.get("front_left_motor"), hardwareMap.dcMotor.get("front_right_motor"), hardwareMap.dcMotor.get("rear_left_motor"), hardwareMap.dcMotor.get("rear_right_motor"));
+      mechDrive.setLinearOp(this);
+
+      intakeSpin = new IntakeSpinner(hardwareMap.servo.get("intake_spinner_left"), hardwareMap.servo.get("intake_spinner_right"));
+
+        waitForStart();
+        idle();
+
+
+        while (opModeIsActive()) {
+
+            mechDrive.stopMotors();
+            mechDrive.driveForward(1, 2.5);
+            sleep(1000);
+            mechDrive.stopMotors();
+
+            intakeSpin
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ACompetitionSkyStone/subsystems/IntakeSpinner.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.ACompetitionSkyStone.subsystems;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+
+
+public class IntakeSpinner {
+
+    //instance variables
+    public Servo intakeServoLeft;
+    public Servo intakeServoRight;
+
+    public double SPD_max = 1;
+    public double SPD_mid = .5;
+    public double SPD_low = .3;
+
+
+
+    // constructor
+    public IntakeSpinner (Servo ISL, Servo ISR) {
+        intakeServoLeft = ISL;
+        intakeServoRight = ISR;
+
+    }
+
+
+    //methods
+    public void stopMotors () {
+        intakeServoLeft.setPosition(.5);
+        intakeServoRight.setPosition(.5);
+    }
+
+    public void SpinForward () {
+        intakeServoLeft.setPosition(1);
+        intakeServoRight.setPosition(1);
+    }
+
+    public void SpinBackward () {
+        intakeServoLeft.setPosition(-1);
+        intakeServoRight.setPosition(-1);
+    }
+
+
+
+
+}
