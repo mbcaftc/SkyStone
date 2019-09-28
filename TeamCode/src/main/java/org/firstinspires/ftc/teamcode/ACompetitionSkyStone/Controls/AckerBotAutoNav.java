b37fe7ab0d82cae52ad9da43cdf8cc53cab22bff
emma.acker <emma.acker@mtbethelchrisitan.org>
2019-09-28 09:43:38 -0400

    Merge branch 'master' of /Users/emma.acker/StudioProjects/SkyStone with conflicts.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ACompetitionSkyStone/Controls/AckerBotAutoNav.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode.ACompetitionSkyStone.Controls;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.Navigation.AutoNavigation;
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.robots.AckerBot;
+
+@Autonomous (name = "AckerBot Auto Navigation")
+public class AckerBotAutoNav extends LinearOpMode {
+
+    public AckerBot Bot = new AckerBot();
+    public AutoNavigation Nav = new AutoNavigation();
+
+    final long  sleepTime = 200;
+    final double maxSpeed = 1;
+    final double highSpeed = .6;
+    final double midSpeed = .5;
+    final double lowSpeed = .3;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        Bot.initRobot(hardwareMap);
+
+        Bot.setLinearOp(this);
+        Nav.setLinearOp(this);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            telemetry.addLine("Sample Skystone");
+            telemetry.update();
+            Nav.sampleSkyStone();
+            sleep(sleepTime);
+
+            telemetry.addLine("Drop off Stone");
+            Nav.dropOffStone();
+            sleep(sleepTime);
+
+            telemetry.addLine("Align with Build Plate");
+            telemetry.update();
+            Nav.alignWithBuildPlate();
+            sleep(sleepTime);
+
+            telemetry.addLine("Re-orient Build Plate");
+            telemetry.update();
+            Nav.reorientBuildPlate ();
+            sleep(sleepTime);
+
+            telemetry.addLine("Go to Sky Stones");
+            telemetry.update();
+            Nav.goToSkyStones();
+            sleep(sleepTime);
+
+
+            idle();
+
+            requestOpModeStop();
+
+        }
+
+        idle();
+
+    }
+
+
+
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ACompetitionSkyStone/Navigation/AutoNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ACompetitionSkyStone/Navigation/AutoNavigation.java
@@ -5,17 +5,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.robots.AckerBot;
 
 
-public abstract class AutoNavigation extends LinearOpMode {
-
-
-    public final long  sleepTime = 200;
-    public final double maxSpeed = 1;
-    public final double highSpeed = .6;
-    public final double midSpeed = .5;
-    public final double lowSpeed = .3;
+public class AutoNavigation {
 
     public AckerBot Bot = new AckerBot();
 
+    final long  sleepTime = 200;
+    final double maxSpeed = 1;
+    final double highSpeed = .6;
+    final double midSpeed = .5;
+    final double lowSpeed = .3;
+
+
+    public LinearOpMode linearOp = null;
+    public void setLinearOp(LinearOpMode linearOp) {
+
+        this.linearOp = linearOp;
+    }
 
     public AutoNavigation() {
 
@@ -24,60 +29,53 @@ public abstract class AutoNavigation extends LinearOpMode {
 
     public void sampleSkyStone () {
 
-        telemetry.addLine("Sample Skystone");
-        telemetry.update();
         Bot.driveForward(highSpeed, 3.4);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         //sample .a sky stone
 
         Bot.driveBackward(highSpeed, .5);              // drive backward with stone
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
     }
 
     public void dropOffStone () {
 
-        telemetry.addLine("Drop off Stone");
-        telemetry.update();
+
         Bot.rotateLeft(highSpeed, 2.5);               // rotate toward build site with stone
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.gyroCorrection(lowSpeed, 85);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.driveBackward(highSpeed, 6.8);                // drive toward build site with the skystone
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         // drop off the sky stone
     }
 
     public void alignWithBuildPlate () {
 
-        telemetry.addLine("Align with Build Plate");
-        telemetry.update();
         Bot.strafeRight(highSpeed, 3.6);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.gyroCorrection(lowSpeed, 85);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.driveBackward(lowSpeed, 1);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
     }
 
     public void reorientBuildPlate() {
 
-        telemetry.addLine("Re-orient Build Plate");
-        telemetry.update();
         //grab build plate
         Bot.rotateRight (midSpeed, 1.5);               // strafe between the wall and the build plate
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.gyroCorrection(lowSpeed, 40);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.driveBackward(midSpeed, 2);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         //release build plate
 
@@ -85,35 +83,33 @@ public abstract class AutoNavigation extends LinearOpMode {
 
     public void goToSkyStones() {
 
-        telemetry.addLine("Go to Sky Stones");
-        telemetry.update();
         Bot.driveForward(highSpeed, 1);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.rotateLeft(midSpeed,1);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.gyroCorrection(lowSpeed, 82);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.driveForward(highSpeed, 8.5);             // strafe out (away from the wall and build site towards the skystones)
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
     }
 
     public void grab2Skystone () {
         Bot.rotateLeft(midSpeed, 2.5);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         Bot.gyroCorrection(lowSpeed, 0);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
         // intake skystone
 
     }
 
     public void dropOff2Skystone () {
         Bot.driveForward(highSpeed, 9.5);
-        sleep(sleepTime);
+        linearOp.sleep(sleepTime);
 
         // drop off skystone with intake
     }
