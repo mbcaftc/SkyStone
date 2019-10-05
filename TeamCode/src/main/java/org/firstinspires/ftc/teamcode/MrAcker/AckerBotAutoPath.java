17a5aa4b50f41b7acb1f9a269ef3993422b170a9
AckerTech <jamie.acker@mtbethelchristian.org>
2019-10-04 21:15:35 -0400

    Updated AckerBot Test Auto cases for passing Ackerbot robot as a parameter.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrAcker/AckerBotAutoPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrAcker/AckerBotAutoPath.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode.MrAcker;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.robots.AckerBot;
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.subsystems.VuforiaWebcam;
 
 @Autonomous (name = "AckerBot Auto Primary Path Test")
 public class AckerBotAutoPath extends AutoNavigation {
 
     public AckerBot Bot = new AckerBot();
-
+    public VuforiaWebcam Cam = new VuforiaWebcam();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -17,13 +18,20 @@ public class AckerBotAutoPath extends AutoNavigation {
         Bot.initRobot(hardwareMap);
         Bot.setLinearOp(this);
 
+        Cam.initCamera(hardwareMap);
+        setLinearOp(this);
+
         waitForStart();
 
         while (opModeIsActive()) {
 
+            telemetry.addLine("Drive Test");
+            telemetry.update();
+            driveTest(Bot);
+
             telemetry.addLine("Sample Skystone");
             telemetry.update();
-            sampleSkyStone(Bot);
+            sampleSkyStone(Bot, Cam);
 
 
             idle();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrAcker/AutoNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MrAcker/AutoNavigation.java
@@ -2,28 +2,31 @@ package org.firstinspires.ftc.teamcode.MrAcker;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.robots.AckerBot;
+import org.firstinspires.ftc.teamcode.ACompetitionSkyStone.subsystems.VuforiaWebcam;
 
 
 public abstract class AutoNavigation extends LinearOpMode {
 
+    public LinearOpMode linearOp = null;
 
-    final long  sleepTime = 1000;
-    final double maxSpeed = 1;
-    final double highSpeed = .6;
-    final double midSpeed = .5;
-    final double lowSpeed = .3;
-
+    public final long  sleepTime = 1000;
+    public final double maxSpeed = 1;
+    public final double highSpeed = .6;
+    public final double midSpeed = .5;
+    public final double lowSpeed = .3;
 
 
     public AutoNavigation() {
 
     }
 
+    public void setLinearOp(LinearOpMode Op) {
 
+        linearOp = Op;
+    }
 
-    public void sampleSkyStone (AckerBot Bot) {
+    public void driveTest (AckerBot Bot) {
 
         Bot.driveForward(highSpeed, 3.4);
         sleep(sleepTime);
@@ -32,5 +35,13 @@ public abstract class AutoNavigation extends LinearOpMode {
         sleep(sleepTime);
     }
 
+    public void sampleSkyStone (AckerBot Bot, VuforiaWebcam Cam) {
+
+        Bot.driveForward(highSpeed, 3.4);
+        sleep(sleepTime);
+
+        Bot.driveBackward(highSpeed, 0.5);              // drive backward with stone
+        sleep(sleepTime);
+    }
 
 }
